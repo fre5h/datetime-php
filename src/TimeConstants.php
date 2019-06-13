77941c4335175437d423f047e80517e7612d140e
Artem Henvald <genvaldartem@gmail.com>
2019-06-14 00:21:49 +0300

    Create TimeConstants.php

---
+++ src/TimeConstants.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is part of the DateTime library
+ *
+ * (c) Artem Henvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Fresh\DateTime;
+
+/**
+ * TimeConstants.
+ *
+ * @author Artem Henvald <genvaldartem@gmail.com>
+ */
+final class TimeConstants
+{
+    public const NUMBER_OF_SECONDS_IN_MINUTE = 60;
+    public const NUMBER_OF_MINUTES_IN_HOUR = 60;
+    public const NUMBER_OF_HOURS_IN_DAY = 24;
+    public const NUMBER_OF_DAYS_IN_WEEK = 7;
+}
