3b838d347a3d07b54e12d58ddec9381b229352d4
Artem Henvald <genvaldartem@gmail.com>
2019-09-12 15:23:58 +0300

    Add more constants

--- src/TimeConstants.php
+++ src/TimeConstants.php
@@ -29,4 +29,53 @@ final class TimeConstants
     public const NUMBER_OF_MONTHS_IN_A_YEAR = 12;
     public const NUMBER_OF_DAYS_IN_A_YEAR = 365;
     public const NUMBER_OF_DAYS_IN_A_LEAP_YEAR = 366;
+
+    public const DAY_MONDAY = 'monday';
+    public const DAY_MONDAY_SHORT = 'mon';
+    public const DAY_TUESDAY = 'tuesday';
+    public const DAY_TUESDAY_SHORT = 'tue';
+    public const DAY_WEDNESDAY = 'wednesday';
+    public const DAY_WEDNESDAY_SHORT = 'wed';
+    public const DAY_THURSDAY = 'thursday';
+    public const DAY_THURSDAY_SHORT = 'thu';
+    public const DAY_FRIDAY = 'friday';
+    public const DAY_FRIDAY_SHORT = 'fri';
+    public const DAY_SATURDAY = 'saturday';
+    public const DAY_SATURDAY_SHORT = 'sat';
+    public const DAY_SUNDAY = 'sunday';
+    public const DAY_SUNDAY_SHORT = 'sun';
+
+    public const MONTH_JANUARY = 'january';
+    public const MONTH_JANUARY_SHORT = 'jan';
+    public const MONTH_FEBRUARY = 'february';
+    public const MONTH_FEBRUARY_SHORT = 'feb';
+    public const MONTH_MARCH = 'march';
+    public const MONTH_MARCH_SHORT = 'mar';
+    public const MONTH_APRIL = 'april';
+    public const MONTH_APRIL_SHORT = 'apr';
+    public const MONTH_MAY = 'may';
+    public const MONTH_MAY_SHORT = 'may';
+    public const MONTH_JUNE = 'june';
+    public const MONTH_JUNE_SHORT = 'jun';
+    public const MONTH_JULY = 'july';
+    public const MONTH_JULY_SHORT = 'jul';
+    public const MONTH_AUGUST = 'august';
+    public const MONTH_AUGUST_SHORT = 'aug';
+    public const MONTH_SEPTEMBER = 'september';
+    public const MONTH_SEPTEMBER_SHORT = 'sep';
+    public const MONTH_OCTOBER = 'october';
+    public const MONTH_OCTOBER_SHORT = 'oct';
+    public const MONTH_NOVEMBER = 'november';
+    public const MONTH_NOVEMBER_SHORT = 'nov';
+    public const MONTH_DECEMBER = 'december';
+    public const MONTH_DECEMBER_SHORT = 'dec';
+
+    public const WINTER_MONTHS = [self::MONTH_DECEMBER, self::MONTH_JANUARY, self::MONTH_FEBRUARY];
+    public const WINTER_MONTHS_SHORT = [self::MONTH_DECEMBER_SHORT, self::MONTH_JANUARY_SHORT, self::MONTH_FEBRUARY_SHORT];
+    public const SPRING_MONTHS = [self::MONTH_MARCH, self::MONTH_APRIL, self::MONTH_MAY];
+    public const SPRING_MONTHS_SHORT = [self::MONTH_MARCH_SHORT, self::MONTH_APRIL_SHORT, self::MONTH_MAY_SHORT];
+    public const SUMMER_MONTHS = [self::MONTH_JUNE, self::MONTH_JULY, self::MONTH_AUGUST];
+    public const SUMMER_MONTHS_SHORT = [self::MONTH_JUNE_SHORT, self::MONTH_JULY_SHORT, self::MONTH_AUGUST_SHORT];
+    public const AUTUMN_MONTHS = [self::MONTH_SEPTEMBER, self::MONTH_OCTOBER, self::MONTH_NOVEMBER];
+    public const AUTUMN_MONTHS_SHORT = [self::MONTH_SEPTEMBER_SHORT, self::MONTH_OCTOBER_SHORT, self::MONTH_NOVEMBER_SHORT];
 }
