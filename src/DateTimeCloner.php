80a1ea3e77341b7b758a539b1f22a65384b681c5
Artem Henvald <genvaldartem@gmail.com>
2022-01-03 14:42:11 +0200

    Update DateTimeCloner.php

--- src/DateTimeCloner.php
+++ src/DateTimeCloner.php
@@ -32,6 +32,7 @@ class DateTimeCloner
      */
     public static function cloneIntoDateTime(\DateTimeInterface $originalDate): \DateTime
     {
+        /** @var $timezone \DateTimeZone|false */
         $timezone = $originalDate->getTimezone();
         $date = \DateTime::createFromFormat(
             self::DATE_FORMAT_FOR_CLONE,
@@ -55,6 +56,7 @@ class DateTimeCloner
      */
     public static function cloneIntoDateTimeImmutable(\DateTimeInterface $originalDate): \DateTimeImmutable
     {
+        /** @var $timezone \DateTimeZone|false */
         $timezone = $originalDate->getTimezone();
         $date = \DateTimeImmutable::createFromFormat(
             self::DATE_FORMAT_FOR_CLONE,
